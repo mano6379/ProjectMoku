91637c664d24d2b18783c19485b977acfbfc02a9
mano6379 <meaola@gmail.com>
2018-02-24 15:05:56 -1000

    Created homescreen swift file and added tableview

---
+++ ProjectMoku/HomeScreenVC.swift
@@ -0,0 +1,46 @@
+//
+//  HomeScreenVC.swift
+//  ProjectMoku
+//
+//  Created by Kumu on 2/24/18.
+//  Copyright © 2018 Kumu. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenVC: UIViewController, UITableViewDataSource, UITabBarDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
